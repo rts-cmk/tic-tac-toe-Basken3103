513f684e66901418601ef04c9e61ad886d9b4756
Christopher S. Vesterby <vesterby@hotmail.dk>
2025-11-20 18:29:08 +0100

    Da der var udfordringer med at køre programmet pga. overflødige filer, fik jeg dem fjernet. Nu er jeg i gang med den funktion, med de forskellige vinder kombinationer.

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,7 +1,7 @@
 import Strike from "./Strike";
 import Tile from "./Tile";
 
-function Board({ tiles, onTileClick, playerTurn }) {
+function Board({ tiles, onTileClick, playerTurn, strikeClass }) {
     return (
     <div className="board">
         <Tile playerTurn={playerTurn} onClick={ ()=> onTileClick(0)} value={tiles[0]} className='right-border bottom-border'/>
@@ -12,7 +12,8 @@ function Board({ tiles, onTileClick, playerTurn }) {
         <Tile playerTurn={playerTurn} onClick={ ()=> onTileClick(5)} value={tiles[5]} className='bottom-border'/>
         <Tile playerTurn={playerTurn} onClick={ ()=> onTileClick(6)} value={tiles[6]} className='right-border'/>
         <Tile playerTurn={playerTurn} onClick={ ()=> onTileClick(7)} value={tiles[7]} className='right-border'/>
-        <Tile playerTurn={playerTurn} onClick={ ()=> onTileClick(8)} value={tiles[8]}/>        <Strike/>
+        <Tile playerTurn={playerTurn} onClick={ ()=> onTileClick(8)} value={tiles[8]}/>  
+        <Strike strikeClass={strikeClass}/>
     </div>
     );
 }
